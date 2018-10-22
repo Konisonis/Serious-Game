e5632f724294a93f5db0d737b0c6732e72aecad5
MaxW <max.waiblinger@t-online.de>
2018-10-22 15:16:07 +0200

    Base Version 0.1

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/CodeFiles/DaytimeSim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaytimeSim : MonoBehaviour {
+
+    public float duration = 1.0F;
+    public Color color0 = Color.red;
+    public Color color1 = Color.blue;
+    public Light lt;
+    private Transform direction;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        direction = lt.transform;
+    }
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+        direction.rotation.x += 1;
+    }
+}
---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/CodeFiles/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotSpeed;
+    public Transform grounddetector;
+
+    private bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, 0, y);
+        transform.Translate(0, z, 0);
+    }
+}
