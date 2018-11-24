64398912a1f8306d2c0d8cb657897ffd0354241f
Konstantin Rosenberg <konsi.rs@web.de>
2018-11-24 12:11:54 +0100

    added pause menu

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUi;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume() {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause() {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
