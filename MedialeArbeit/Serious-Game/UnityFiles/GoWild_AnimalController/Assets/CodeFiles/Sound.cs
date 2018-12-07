c0a8254f00b1a4c9778aa8381c5206cbb2466154
Joffrey Schneider <joffrey.schneider@student.reutlingen-university.de>
2018-12-07 15:42:07 +0100

    Simple ball sound added

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/CodeFiles/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound : MonoBehaviour {
+
+    public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = clip;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        if (collision.gameObject.name == "Dog")
+        {
+            source.volume = 1f;
+        } else
+        {
+            source.volume = (collision.relativeVelocity.magnitude-15) / 8f;
+        }
+        
+        source.Play();
+    }
+}
