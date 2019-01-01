7eb95a183f4082acd0f248881ee3caadac5bb0b5
SchrammR <robinschramm@gmx.de>
2019-01-01 22:45:06 +0100

    added Dog Animator with wedel animation

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/Animations/AnimationEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour {
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			animator.SetBool ("Active", true);
+		} else {
+			animator.SetBool ("Active", false);
+		}
+	}
+}
