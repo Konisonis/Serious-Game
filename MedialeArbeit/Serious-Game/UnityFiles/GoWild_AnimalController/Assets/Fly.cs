eac9a764592a0176e43d22cccf7a9da9d9a9ec21
Heerscher <ravell1993@yahoo.de>
2019-01-03 19:39:38 +0100

    Bisschen map weitergebaut, neue Texturen, neue Objekte

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/Fly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+	public float horizontalSpeed = 0;
+	public float verticalSpeed = 1.5f;
+	public float amplitude = 0.5f;
+
+	public Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+		tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		tempPosition.x += horizontalSpeed;
+		tempPosition.y = 72 + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+		transform.position = tempPosition;
+	}
+}
