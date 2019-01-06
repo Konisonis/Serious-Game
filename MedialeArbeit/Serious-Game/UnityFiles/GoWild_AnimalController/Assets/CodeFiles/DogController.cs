3a8b9e047732b275435ea94d503e51356dedf8be
SchrammR <robinschramm@gmx.de>
2019-01-06 19:58:25 +0100

    added Dog Head rotation, deleted corrupted assets

--- MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/CodeFiles/DogController.cs
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/CodeFiles/DogController.cs
@@ -15,47 +15,80 @@ public class DogController : Controller{
 
     private AudioSource walksound;
 
+	public Transform neckBone;
+	private float yCameraRotation, zCameraRotation, yNeckRotation, zNeckRotation;
+
 	void Start () {
         active = true;
         canDig = false;
 
         walksound = GetComponent<AudioSource>();
+
+		yCameraRotation = 180;
+		zCameraRotation = 0;
+		yNeckRotation = neckBone.transform.rotation.y;
+		zNeckRotation = neckBone.transform.rotation.z;
+
+		Mesh mesh = GetComponentInChildren<MeshFilter> ().mesh;
+		Debug.LogWarning(mesh.vertices);
+
 	}
 
 	void Update () {
         if (active)
-        {
-            var rot = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
-            var trans = Input.GetAxis("Vertical") * speed;
-			Vector3 facingDirection = transform.TransformDirection (new Vector3 (0, trans, 0));
-
-			if (leftJoyCon != null && rightJoyCon != null) {
-				if (leftJoyCon.isWalking () && rightJoyCon.isWalking ()) {
-					trans = 1 * Time.deltaTime * speed;
-					transform.Translate (0, trans, 0);
-				}
-			}
-
-            transform.Rotate(0, 0, rot);
-            //transform.Translate(0, trans, 0);
-			GetComponent<Rigidbody>().MovePosition(transform.position + facingDirection * Time.deltaTime);
-
-            // Sound of walking
-            if (trans > 0.5)
-            {
-                if (!walksound.isPlaying) walksound.Play(0);
-            }
-            else walksound.Stop();
-            // ----------------
-
-            if (Input.GetButtonDown("Jump") && canDig)
-            {
-                Vector3 pos = target.GetComponent<Transform>().position;
-                pos.y += 0.5f;
-                target.GetComponent<Transform>().position = pos;
-            }
+		{
+			move();
+			dig();
+			lookAround();
         }
     }
+
+	void move(){
+		var rot = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+		var trans = Input.GetAxis("Vertical") * speed;
+		Vector3 facingDirection = transform.TransformDirection (new Vector3 (0, trans, 0));
+
+		if (leftJoyCon != null && rightJoyCon != null) {
+			if (leftJoyCon.isWalking () && rightJoyCon.isWalking ()) {
+				trans = 1 * Time.deltaTime * speed;
+				transform.Translate (0, trans, 0);
+			}
+		}
+
+		transform.Rotate(0, 0, rot);
+		GetComponent<Rigidbody>().MovePosition(transform.position + facingDirection * Time.deltaTime);
+
+		// Sound of walking
+		if (trans > 0.5)
+		{
+			if (!walksound.isPlaying) walksound.Play(0);
+		}
+		else walksound.Stop();
+		// ----------------
+	}
+
+	void dig(){
+		if (Input.GetButtonDown("Jump") && canDig)
+		{
+			Vector3 pos = target.GetComponent<Transform>().position;
+			pos.y += 0.5f;
+			target.GetComponent<Transform>().position = pos;
+		}
+	}
+
+	void lookAround(){
+		yCameraRotation += 2 * Input.GetAxis ("Mouse X");
+		zCameraRotation -= 2 * Input.GetAxis ("Mouse Y");
+		yNeckRotation += 2 * Input.GetAxis ("Mouse X");
+		zNeckRotation -= 2 * Input.GetAxis ("Mouse Y");
+
+
+		Vector3 cameraRotation = new Vector3 (zCameraRotation, yCameraRotation, 0f);
+		GetComponentInChildren<Camera> ().transform.eulerAngles = cameraRotation;
+
+		Vector3 neckRotation = new Vector3 (-yNeckRotation, 0f, -zNeckRotation);
+		neckBone.transform.localEulerAngles = neckRotation;
+	}
 
     void OnTriggerEnter(Collider other)
     {
