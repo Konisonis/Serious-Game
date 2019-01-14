a9fae6e3b39663cf9bb247d5079aa3ae179353ad
Robin Schramm <robinschramm@gmx.de>
2019-01-14 11:48:14 +0100

    digging box fixed

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/notes.cs
@@ -0,0 +1,50 @@
+﻿/*
+TODO:
+Fliegen werte anpassen
+ - stärker nach vorn
+ - schwächer nach oben
+ - weniger gewicht
+
+Graben bewegung anpassen
+ - nach vorn und hinten bewegen
+
+Springen auf Buttons legen
+Switchen auf Buttons legen
+
+Fliegen rotation anpassen, Person rotation mit einbeziehen
+muss einfach nur waagerecht sein
+z-wert ist bei gewisser rotation zu niedrig und man hebt zu sehr ab
+
+Menü einbinden
+
+Graben registrierung größer machen
+
+Ente schwimmen lassen
+
+
+
+===============
+
+Laufen und Fliegen funktioniert manchmal perfekt, manchmal gar nicht
+
+
+
+
+Fragebogen:
+
+- Hattest du Spaß?
+	- hat Spaß gemacht 
+	- 
+- was hat dir gefallen
+	- Pyramide, fliegen falls es geht
+	- 
+- was hat dir nicht gefallen / Verbesserungsvorschläge
+	- fliegen
+	- 
+- ist dir schlecht geworden / übelkeit?
+	- ganz am anfang kurz
+	- 
+
+
+
+ */
