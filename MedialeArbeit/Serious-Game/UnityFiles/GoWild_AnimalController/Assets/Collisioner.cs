1ab30e589664198991f4c8e61bfac9d56a5c0643
Heerscher <ravell1993@yahoo.de>
2019-01-18 17:43:26 +0100

    Waterfloor for swimming feeling

---
+++ MedialeArbeit/Serious-Game/UnityFiles/GoWild_AnimalController/Assets/Collisioner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisioner : MonoBehaviour {
+
+	public AudioSource waterin;
+	public AudioSource waterout;
+
+	// Use this for initialization
+	void Start () {
+		this.waterin = GetComponent<AudioSource>();
+		this.waterout = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Dog" || other.gameObject.tag == "Duck") {
+			this.waterin.Play();
+
+			Destroy (other.gameObject);
+		}
+	}
+
+	private void OnCollisionExit(Collision other) {
+		if (other.gameObject.tag == "Dog" || other.gameObject.tag == "Duck") {
+			this.waterout.Play();
+
+			Destroy (other.gameObject);
+		}
+	}
+}
